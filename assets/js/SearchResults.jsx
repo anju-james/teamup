0bb8c117b4fe88b408d65dd39f95eb28faa7bddb
anju <james.an@husky.neu.edu>
2018-12-05 15:00:26 -0500

    spacing

--- assets/js/SearchResults.jsx
+++ assets/js/SearchResults.jsx
@@ -6,6 +6,8 @@ import {connect} from "react-redux";
 import {Link} from 'react-router-dom';
 import FuzzySearch from 'fuzzy-search';
 import {ToastContainer, toast, Flip} from 'react-toastify';
+import {USER_JOIN_GROUP, USER_LEAVE_GROUP} from "./store";
+import store from "./store";
 
 
 class SearchResultsView extends React.Component {
@@ -46,6 +48,7 @@ class SearchResultsView extends React.Component {
                                 <SingleResult key={group.id} user_info={this.props.user_info} group_id={group.id}
                                               title={group.title} description={group.description}
                                               date={group.date} time={group.time} location={group.location}
+                                              attendees={group.attendees}
                                 />
                             </div>)}
                     </div>
@@ -55,7 +58,7 @@ class SearchResultsView extends React.Component {
                 <br></br>
                 <br></br>
                 <br></br>
-
+                <br></br>
                 <br></br>
 
                 <footer className="page-footer orange footer-copyright">
@@ -75,18 +78,87 @@ class SingleResult extends React.Component {
         super(props);
     }
 
-    handleClick(group_id) {
+    /*
+    handleClickJoin(group_id) {
         if (this.props.user_info.username) {
             toast.success("You have joined this study group!", {
                 position: toast.POSITION.TOP_RIGHT
             });
             store.dispatch({type: USER_JOIN_GROUP, group_id: group_id, fullname: this.props.user_info.fullname})
         } else {
-            toast.warn("Login before you join a group");
+            toast.warn("Login before you can join a group");
         }
     }
 
+    handleClickLeave(group_id) {
+        if (this.props.user_info.username) {
+            toast.warn("You have left this study group!", {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            store.dispatch({type: USER_LEAVE_GROUP, group_id: group_id, fullname: this.props.user_info.fullname})
+        } else {
+            toast.warn("Login before you can leave a group");
+        }
+    }
+
+    renderActionButton(user_already_joined, matching_group) {
+        matching_group = this.props.group_id;
+
+        if (user_already_joined) {
+            return (<div className=" center-align">
+                <button onClick={(e) => this.handleClickLeave(matching_group.id, e)}
+                        className="waves-effect black-text waves-light btn yellow lighten-1">
+                    Leave
+                </button>
+                <ToastContainer
+                    transition={Flip}
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnVisibilityChange={false}
+                    draggable={false}
+                    pauseOnHover={false}
+                />
+            </div>);
+        } else {
+            return (<div className=" center-align">
+                <button onClick={(e) => this.handleClickJoin(matching_group.id, e)}
+                        className="waves-effect waves-light btn green lighten-1">
+                    Join
+                </button>
+                <ToastContainer
+                    transition={Flip}
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnVisibilityChange={false}
+                    draggable={false}
+                    pauseOnHover={false}
+                />
+            </div>);
+        }
+    }
+    */
+
+
     render() {
+        /*
+        const group_id = this.props.group_id;
+        let matching_group = this.props.study_groups.find((group) => group.id == group_id)
+
+        if (matching_group.attendees.includes(this.props.user_info.username)){
+            user_already_joined = true;
+        }
+
+        let action_buttons = this.renderActionButton(user_already_joined, matching_group);
+        */
+
         return (
             <div className="row">
                 <div className="col s12 m6">
@@ -107,7 +179,7 @@ class SingleResult extends React.Component {
                                     <span>{this.props.time} </span>
                                     <i className="material-icons blue-text">location_on</i>
                                     <span>{this.props.location} </span>
-                                        <button onClick={(e) => this.handleClick(this.props.group_id, e)} className="right waves-effect waves-light btn green lighten-1">Join</button>
+                                    <button onClick={(e) => this.handleClick(this.props.group_id, e)} className="right waves-effect waves-light btn green lighten-1">Join</button>
                                     <Link to={"/group_details/" + this.props.group_id}
                                           className="right waves-effect orange-text text-accent-4 btn-flat">See More</Link>
                                 </div>
